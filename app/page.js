bf41489bda0b95c01014f00ee001b2ad6916f10a
sebapala42-svg <sebapala42@gmail.com>
2026-03-30 01:21:12 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -733,8 +733,8 @@ export default function Home() {
                 </div>
               </div>
             </div>
-          )}
-        </div>
+          </>
+        )}
       </main>
     </div>
   );
